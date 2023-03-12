0ae15b1aef6cf20f2ff0ccceb0ac129a274b25f0
Zain Muhammad <zainzoya03@gmail.com>
2023-03-12 04:08:23 +0000

    create outline for main class interaction with config creation

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <config.h>
+
+int main(int argc, char *argv[]) { 
+    struct config configuration;
+    if (argc > 1) { 
+        // construct configuration from cmd line arguments
+    } else {
+        // construct configuration from default fn
+    }
+    // ...
+}
