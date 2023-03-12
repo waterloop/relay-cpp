78459f495b9917caa03245458597a2378fefb299
Zain Muhammad <zainzoya03@gmail.com>
2023-03-12 04:38:31 +0000

    write the default configuration function definition

---
+++ src/config.c
@@ -0,0 +1,17 @@
+#include <config.h>
+
+struct config default() { 
+    struct config c = {
+        .sockfd = socket(AF_INET, SOCK_STREAM, 0),
+        .tcp_address = {
+            .sin_family = AF_INET,
+            .sin_port = htons(8080),
+            .sin_addr.s_addr = htonl(INADDR_ANY)
+        }
+    }
+    // int flag = 1;
+    // setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag));
+    bind(c.sockfd, (struct sockaddr *)&c.tcp_address, sizeof(c.tcp_address));
+    return c;
+}
+
