d1dcd4b1fe2100ed40015c471d1af6794977b520
Zain Muhammad <zainzoya03@gmail.com>
2023-03-12 04:09:36 +0000

    create config struct and function declerations

---
+++ src/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <netinet/in. h>
+
+struct config {
+    int tcp_sockfd;
+    struct sockaddr_in tcp_address;
+};
+
+struct config default();
+
+struct config from_args()
+
+#endif // CONFIG_H
