b82c91fb859f1c475b5a2b8aed548a1d8e42832d
Zain Muhammad <zainzoya03@gmail.com>
2023-03-12 05:34:00 +0000

    add necessary headers

--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
-#include <config.h>
+#include "config.h"
+#include "run_threads.h"
 
 int main(int argc, char *argv[]) { 
     struct config configuration;
     if (argc > 1) { 
         // construct configuration from cmd line arguments
     } else {
-        // construct configuration from default fn
+        configuration = config_default();
     }
-    // ...
+    run_threads(configuration);
 }
