a312efd6caafc48e5b2261f421e7e59df41c3c1e
Zain Muhammad <zainzoya03@gmail.com>
2023-03-12 05:37:39 +0000

    update function arguments to be more clear

--- src/config.c
+++ src/config.c
@@ -1,17 +1,18 @@
-#include <config.h>
+#include "config.h"
+#include <sys/socket.h>
 
-struct config default() { 
+struct config config_default(void) { 
     struct config c = {
-        .sockfd = socket(AF_INET, SOCK_STREAM, 0),
+        .tcp_sockfd = socket(AF_INET, SOCK_STREAM, 0),
         .tcp_address = {
             .sin_family = AF_INET,
             .sin_port = htons(8080),
             .sin_addr.s_addr = htonl(INADDR_ANY)
         }
-    }
+    };
     // int flag = 1;
     // setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag));
-    bind(c.sockfd, (struct sockaddr *)&c.tcp_address, sizeof(c.tcp_address));
+    bind(c.tcp_sockfd, (struct sockaddr *)&c.tcp_address, sizeof(c.tcp_address));
     return c;
 }
 
--- src/config.h
+++ src/config.h
@@ -1,14 +1,14 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include <netinet/in. h>
+#include <netinet/in.h>
 
 struct config {
     int tcp_sockfd;
     struct sockaddr_in tcp_address;
 };
 
-struct config default();
+struct config config_default(void);
 
 struct config from_args(char *args[]);
 
